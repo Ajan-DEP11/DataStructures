477b1636b0011da648d5b3d9421cf1f7efd1dfdc
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-18 20:35:52 +0530

    first

--- Documents/DEP - Practice/Arrays/Demo1.java
+++ Documents/DEP - Practice/Arrays/Demo1.java
@@ -1,8 +1,8 @@
-import java.util.Vector;
+// import java.util.Vector;
 
-public class Demo1 {
-    public static void main(String[] args) {
+// public class Demo1 {
+//     public static void main(String[] args) {
         
-        Vector< Integer> myVector = new Vector<>(0);
-    }
-}
+//         Vector< Integer> myVector = new Vector<>(0);
+//     }
+// }
---
+++ Documents/DEP - Practice/Arrays/VectorDemo.java
@@ -0,0 +1,12 @@
+import java.util.Vector;
+
+public class VectorDemo {
+    public static void main(String[] args) {
+        Vector<Integer> vector = new Vector<>();
+
+        vector.add(1);
+        vector.add(2);
+
+        System.out.println(vector);
+    }
+}
