1b46454679a1bd2c5d6e6b2605144259ecaaf65b
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-20 10:17:22 +0530

    third

--- Documents/DEP11/Phase-1/java-se/Regular Expressions/Demo2.java
+++ Documents/DEP11/Phase-1/java-se/Regular Expressions/Demo2.java
@@ -1,3 +1,22 @@
+import java.util.Scanner;
+
 public class Demo2 {
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.print("Enter your Contact Number: ");
+        String contact = scanner.nextLine().strip();
+        //077-1234567 +9477-1234567
+
+        if(contact.startsWith("0")){
+            for(int i=0; i<3;i++){
+                if(!Character.isDigit(contact.charAt(i))){
+                    System.out.println("Invalid contact number");
+                    return;
+                }
+
+            }
+        }
+        
+    }
     
 }
