a99492228ba7b1b3067386b8a15b25f97f798328
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-20 10:47:13 +0530

    fifth

--- Documents/DEP11/Phase-1/java-se/Regular Expressions/Demo1RegEx.java
+++ Documents/DEP11/Phase-1/java-se/Regular Expressions/Demo1RegEx.java
@@ -4,8 +4,15 @@ public class Demo1RegEx {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        while(true){
         System.out.print("Enter your NIC: ");
-        String nic = scanner.nextLine();
+        String nic = scanner.nextLine().strip();
+       if( nic.matches("\\d{9}[Vv]")){
+        System.out.println("valid NIC");
+       }else{
+        System.out.println("Inavalid NIC");
+       }
+    }
     }
     
 }
