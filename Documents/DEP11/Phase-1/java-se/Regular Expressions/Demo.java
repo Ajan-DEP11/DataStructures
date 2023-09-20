cafc8b3ffbbca7c47a71a9ab046e9acd6c9b2149
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-20 10:05:50 +0530

    second

--- Documents/DEP11/Phase-1/java-se/Regular Expressions/Demo.java
+++ Documents/DEP11/Phase-1/java-se/Regular Expressions/Demo.java
@@ -5,7 +5,8 @@ public class Demo {
 
     private static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-
+        
+        outer:
        while(true){ 
         
         System.out.print("Enter your NIC: ");
@@ -13,19 +14,19 @@ public class Demo {
 
         if(nic.length() !=10){
             System.out.println("invalid nic");
-            return;
+            continue;
         }
 
         for(int i =0; i<9; i++){
             if(!Character.isDigit(nic.charAt(i))){
                 System.out.println("Inavalid");
-                return;
+                continue outer;
             }
         }
 
         if(!(nic.endsWith("v") || nic.endsWith("V"))){
             System.out.println("Invalid");
-            return;
+           continue;
         }
         System.out.println("valid NIC");
         }
